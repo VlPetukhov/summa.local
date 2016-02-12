5ea8cbc44abe27f1f6f4198ece4b48f420f0822c
Vl_Petukhov <vpetuhov@devvela.com>
2016-02-12 16:45:11 +0500

    Slides bootstrap behaviour tuning.

--- views/SiteController/gallery.php
+++ views/SiteController/gallery.php
@@ -3,14 +3,14 @@
  * @var models\Image[] $gallery
  */
 ?>
-<div class="container col-sm-8 col-sm-offset-2">
+<div class="container col-lg-10 col-lg-offset-1 col-md-10 col-md-offset-1  col-sm-10 col-sm-offset-1 col-xs-12 col-xs-offset-0">
     <h1>Your Gallery</h1>
     <a href="/site/upload/">Upload new image</a>
     <a href="/user/logout/" id="gallery-logout">Logout</a>
     <hr>
     <p id="gallery-is-empty" class="<?=(empty($gallery))?'':'hidden';?>">Your Gallery is empty. Start fill your Gallery with images right now and create amazing image collection!</p>
 <?php foreach ($gallery as $img) :?>
-    <div class="image-container col-md-6">
+    <div class="image-container col-lg-4 col-md-6 col-sm-6 col-xs-12">
         <div>
             <p class="image-date">Added to gallery: <?=date('d.m.Y', $img->date)?></p>
             <form action="/site/delete" method="POST" class="image-delete-frm">
@@ -18,7 +18,7 @@
                 <input type="submit" value="Delete">
             </form>
         </div>
-        <img class="preview-img col-md-10" src="<?= '/' . $img->path;?>">
+        <img class="preview-img col-lg-10 col-lg-offset-1 col-md-10 col-md-offset-1 col-sm-10 col-sm-offset-1 col-xs-10 col-xs-offset-1" src="<?= '/' . $img->path;?>">
         <p class="image-descr"><?= $img->getDescription(); ?></p>
     </div>
 <?php endforeach;?>
